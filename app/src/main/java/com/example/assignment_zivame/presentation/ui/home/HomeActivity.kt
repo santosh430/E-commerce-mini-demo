94fc3e1ddaab126975b98c5df78753a7f621817d
Santosh Bhatt <santosbhatt15@gmail.com>
2022-05-19 16:12:19 +0530

    Made Adapter for recycler view for home screen , added Glide dependencies and coded resource file for Adapter .

--- app/src/main/java/com/example/assignment_zivame/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/assignment_zivame/presentation/ui/home/HomeActivity.kt
@@ -3,29 +3,52 @@ package com.example.assignment_zivame.presentation.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assignment_zivame.ProductApplication
 import com.example.assignment_zivame.R
 import com.example.assignment_zivame.data.repo.ProductRepository
+import com.example.assignment_zivame.databinding.ActivityHomeBinding
+import com.example.assignment_zivame.networkcalls.dataclassapi.Product
 import com.example.assignment_zivame.presentation.viewmodel.home.HomeViewModel
 import com.example.assignment_zivame.presentation.viewmodel.home.MainViewModelFactory
 
 class HomeActivity : AppCompatActivity() {
     lateinit var repository: ProductRepository
     lateinit var mViewModel: HomeViewModel
+    lateinit var binding:ActivityHomeBinding
+    lateinit var myadapter:HomeRecylerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_home)
+        supportActionBar?.hide()
 
         // initializing viewModel
         repository  = ( applicationContext as ProductApplication).productRepository
         mViewModel = ViewModelProvider(this,
             MainViewModelFactory(repository))[HomeViewModel::class.java]
 
+        //setting recycler view
+        setRecyclerView()
+
+        // observing product data from api through viewModel
         mViewModel.productLiveData.observe({ lifecycle }, {
             Log.d("TAG",it.toString())
+
+            myadapter.playerList(it as MutableList<Product>)
+            binding.rvGadgets.adapter?.notifyDataSetChanged()
         })
+
+    }
+
+    private fun setRecyclerView() {
+        binding.rvGadgets.apply {
+            layoutManager= LinearLayoutManager(this@HomeActivity)
+            myadapter = HomeRecylerAdapter(this@HomeActivity)
+            adapter=myadapter
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/assignment_zivame/presentation/ui/home/HomeRecylerAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.assignment_zivame.presentation.ui.home
+
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.assignment_zivame.databinding.AdapterHomeRecyclerBinding
+import com.example.assignment_zivame.networkcalls.dataclassapi.Product
+
+class HomeRecylerAdapter(private val homeActivity: HomeActivity) : RecyclerView.Adapter<HomeRecylerAdapter.ViewHolder>() {
+
+    var list= mutableListOf<Product>()
+
+    inner class ViewHolder(val binding: AdapterHomeRecyclerBinding): RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return (ViewHolder(
+            AdapterHomeRecyclerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false)
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        Glide.with(homeActivity)
+            .load(list[position].image_url)
+            .into(holder.binding.ivProductImage)
+        holder.binding.tvProductName.text=list[position].name
+        holder.binding.tvProductPrice.text=list[position].price
+        holder.binding.tvProductRating.text= list[position].rating.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+    fun playerList(list1:MutableList<Product>){
+        list.clear()
+        list=list1
+        Log.d("myTAG","list in adapter $list1")
+        notifyDataSetChanged()
+
+    }
+
+}
