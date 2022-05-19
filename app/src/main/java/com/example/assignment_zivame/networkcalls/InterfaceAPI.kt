14970623d1953235eb4c14e6dd637c66b7384d93
Santosh Bhatt <santosbhatt15@gmail.com>
2022-05-19 08:12:17 +0530

    coded InterfaceAPI.kt and RetrofitHelper.kt

--- app/src/main/java/com/example/assignment_zivame/networkcalls/InterfaceAPI.kt
+++ app/src/main/java/com/example/assignment_zivame/networkcalls/InterfaceAPI.kt
@@ -1,4 +1,11 @@
 package com.example.assignment_zivame.networkcalls
 
+import com.example.assignment_zivame.networkcalls.dataclassapi.DataClassAPI
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface InterfaceAPI {
+
+    @GET("/nancymadan/assignment/db")
+    suspend fun getProducts():Response<DataClassAPI>
 }
--- app/src/main/java/com/example/assignment_zivame/networkcalls/RetrofitHelper.kt
+++ app/src/main/java/com/example/assignment_zivame/networkcalls/RetrofitHelper.kt
@@ -1,5 +1,15 @@
 package com.example.assignment_zivame.networkcalls
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
+    const val BASE_URL="https://my-json-server.typicode.com/"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            // we need to add converter factory to
+            // convert JSON object to Java object
+            .build()
+    }
 }
