609ff11f3b5b2a9016567c573f8b72507baed061
Santosh Bhatt <santosbhatt15@gmail.com>
2022-05-20 05:53:20 +0530

    (Final Commit) Added navigation from OrderPlacedFragment.kt to HomeActivity.kt on click listen of the button "continue shopping" and added nohistory = true in manifest for the activity to clear all the backstack.

--- app/src/main/java/com/example/assignment_zivame/presentation/ui/orderplaced/OrderPlacedFragment.kt
+++ app/src/main/java/com/example/assignment_zivame/presentation/ui/orderplaced/OrderPlacedFragment.kt
@@ -1,21 +1,33 @@
 package com.example.assignment_zivame.presentation.ui.orderplaced
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.assignment_zivame.R
+import com.example.assignment_zivame.databinding.FragmentOrderPlacedBinding
+import com.example.assignment_zivame.presentation.ui.home.HomeActivity
 
 
 class OrderPlacedFragment : Fragment() {
 
+    lateinit var binding:FragmentOrderPlacedBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentOrderPlacedBinding.inflate(inflater)
+
+        //getting back to the HomeActivity after click
+        binding.btnContinueShopping.setOnClickListener {
+            val intent = Intent(context,HomeActivity::class.java)
+            startActivity(intent)
+
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_placed, container, false)
+        return binding.root
     }
 
 }
